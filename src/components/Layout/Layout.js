10215a8c210234c264a447c946c2164a1b5d6879
simongartner-ourliving <simon.gartner@ourliving.se>
2023-04-03 19:40:32 +0200

    responsive layout fix

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import Header from '../Header/Header'
 import './layout.css'
 
 const Layout = ({ children }) => {
+    document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width');
     return (
         <div>
             <Header></Header>
