a02c0853b116f842be45dd9d919e03a6f4f995b4
simongartner-ourliving <simon.gartner@ourliving.se>
2023-04-03 19:41:10 +0200

    cleanup

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,7 +3,6 @@ import Header from '../Header/Header'
 import './layout.css'
 
 const Layout = ({ children }) => {
-    document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width');
     return (
         <div>
             <Header></Header>
