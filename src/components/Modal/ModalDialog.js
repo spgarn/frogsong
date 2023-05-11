c710ddd04226a21c89fba229dc6f663f759e4f69
spgarn <simon.gartner@telia.com>
2023-05-11 21:19:03 +0200

    Added button and CROSS on modal

--- src/components/Modal/ModalDialog.js
+++ src/components/Modal/ModalDialog.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import Modal from 'react-modal';
 import './modalDialog.css'
+import Button from '../Button/Button';
 
 
 Modal.setAppElement('#___gatsby');
 
 const ModalDialog = ({ isOpen, onRequestClose, children }) => {
 
+
     return (
         <Modal
             className='styled-modal'
             onRequestClose={onRequestClose}
             isOpen={isOpen}
-        >{children}</Modal>
+        >
+            <span onClick={onRequestClose} className='modal-cross-close'>+</span>
+            {children}
+            <div className='modal-button'>
+                <Button onClick={onRequestClose}>Close</Button>
+            </div>
+        </Modal>
     )
 }
 
