c0a55dfd524601487e6e6bb6a846b1f50699b4e3
Simon Gärtner <simon.gartner@telia.com>
2023-05-28 00:48:06 +0200

    Fix padding and dark text

--- src/components/Posts/PostCardNew.js
+++ src/components/Posts/PostCardNew.js
@@ -10,8 +10,8 @@ const PostCardNew = ({ post, to, createdAt }) => {
 
     return (
         <Link to={to} key={post.id} className='post-card-new'>
-            <H3 style={{ gridArea: 'title' }}>{post.title}</H3>
-            <H4 style={{ gridArea: 'description' }}>{post.shortText}</H4>
+            <H3 className={'dark-text'} style={{ gridArea: 'title' }}>{post.title}</H3>
+            <H4 className={'dark-text'} style={{ gridArea: 'description' }}>{post.shortText}</H4>
             <div className='post-card-author' style={{ gridArea: 'author' }}>
                 <img alt="Author" style={{ borderRadius: '50%', width: '50px', gridArea: 'image', alignSelf: 'center' }} src={`${post?.author?.fields?.profile?.fields?.file?.url}?w=200&h=200&fit=fill&f=center`}></img>
                 <H4 style={{ color: 'var(--complimentary)' }}>{post?.author?.fields?.name}</H4>
