854cfe6c176abd1d26445dd6ddd07ee8118dd304
Simon Gärtner <simon.gartner@positiongreen.com>
2023-09-26 09:05:10 +0200

    fix

--- src/components/CardSlider/CardSlider.js
+++ src/components/CardSlider/CardSlider.js
@@ -10,8 +10,8 @@ const CardSlider = ({ posts }) => {
     <div className='other-news-cards'>
       <i onClick={() => currentView > 0 && setCurrentView(prev => prev - 1)} className="fa fa-arrow-left fa-lg card-slider-arrow" aria-hidden="true"></i>
       <NewsCard to={`/news/${posts[currentView]?.slug}`} createdAt={posts[currentView].createdAt} post={posts[currentView]}></NewsCard>
-      <NewsCard to={`/news/${posts[currentView + 1].slug}`} createdAt={posts[currentView + 1].createdAt} post={posts[currentView + 1]}></NewsCard>
-      <NewsCard to={`/news/${posts[currentView + 2].slug}`} createdAt={posts[currentView + 2].createdAt} post={posts[currentView + 2]}></NewsCard>
+      {posts.length > 1 && <NewsCard to={`/news/${posts[currentView + 1].slug}`} createdAt={posts[currentView + 1].createdAt} post={posts[currentView + 1]}></NewsCard>}
+      {posts.length > 2 &&<NewsCard to={`/news/${posts[currentView + 2].slug}`} createdAt={posts[currentView + 2].createdAt} post={posts[currentView + 2]}></NewsCard>}
       <i onClick={() => currentView < posts.length - 3 && setCurrentView(prev => prev + 1)} style={{ justifySelf: 'end' }} className="fa fa-arrow-right fa-lg card-slider-arrow" aria-hidden="true"></i>
     </div>
   )
