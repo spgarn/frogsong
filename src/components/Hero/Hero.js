4f6ce936332ccc47da7052201f58f81578a444cc
Simon Gärtner <simon.gartner@positiongreen.com>
2024-05-18 12:46:14 +0200

    Update hero to contact

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,11 +13,16 @@ const Hero = ({ heroData, ...rest }) => {
                 <div className='hero-text-wrapper'>
 
 
-                    {heroData.fields.heading && <H1 className={'no-text-shadow'} style={{ padding: '0px 12px' }}>{heroData.fields.heading}</H1>}
+                    {/* {heroData.fields.heading && <H1 className={'no-text-shadow'} style={{ padding: '0px 12px' }}>{heroData.fields.heading}</H1>}
                     {heroData.fields.subhead && <H2 className={'no-text-shadow'} style={{ padding: '0px 12px', textTransform: 'uppercase' }}>{heroData.fields.subhead}</H2>}
                     <a target='_blank' href={heroData.fields.buttonLink} rel="noreferrer" style={{ padding: '0px 12px' }}>
                         <Button style={{ width: '200px', borderRadius: '2px' }}>Try it out now!</Button>
-                    </a>
+                    </a> */}
+                  <H1 className={'no-text-shadow'} style={{ padding: '0px 12px' }}>We are now looking for new challenges and collaborations.</H1>
+                  <H2 className={'no-text-shadow'} style={{ padding: '0px 12px' }}>Contact us below for more information!</H2>
+                     <a href={"/contact"} rel="noreferrer" style={{ padding: '0px 12px' }}>
+                        <Button style={{ width: '200px', borderRadius: '2px' }}>Contact us here!</Button>
+                    </a> 
                 </div>
 
             </div>
